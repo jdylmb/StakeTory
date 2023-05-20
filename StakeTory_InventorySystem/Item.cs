21661835d61f4ddd2834f89f95d3aabc96d9afa7
Judy Baguinang <21-1-00611@vsu.edu.ph>
2023-05-20 19:46:24 +0800

    First commit

---
+++ StakeTory_InventorySystem/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StakeTory_InventorySystem
+{
+    public class Item
+    {
+        public string Code { get; }
+        public string Name { get; }
+        public int Quantity { get; set; }
+        public double Price { get; }
+
+        public Item(string code, string name, int quantity, double price)
+        {
+            Code = code;
+            Name = name;
+            Quantity = quantity;
+            Price = price;
+        }
+    }
+}
