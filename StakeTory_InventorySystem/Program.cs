dee767510e913856ed1355ac3f3bb6d7c31f6bf5
Judy Baguinang <21-1-00611@vsu.edu.ph>
2023-06-03 23:08:15 +0800

    connect to backend

--- StakeTory_InventorySystem/Program.cs
+++ StakeTory_InventorySystem/Program.cs
@@ -1,38 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using MySql.Data.MySqlClient;
-
-using System;
-using System.Collections.Generic;
-using MySql.Data.MySqlClient;
+﻿
 using StakeTory_InventorySystem;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string connectionString = "Server=localhost;Database=inventory;Uid=root;Pwd";
-        InventorySystem inventorySystem = new InventorySystem(connectionString);
-
-        // Login
-        Console.WriteLine("Please enter your username:");
-        string username = Console.ReadLine();
-        Console.WriteLine("Please enter your password:");
-        string password = Console.ReadLine();
-
-        bool loggedIn = inventorySystem.Login(username, password);
-
-        if (loggedIn)
-        {
-            Console.WriteLine("Login successful!");
-
-            // Perform inventory operations
-            // ...
-        }
-        else
-        {
-            Console.WriteLine("Login failed. Invalid username or password.");
-        }
 
         Console.ReadLine();
     }
